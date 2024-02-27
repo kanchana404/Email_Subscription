70530dd89ee42247b701c3031198c1e77e00d583
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-02-27 12:11:29 +0530

    last

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-const scriptURL = '<Your google sheet script web app link>';
-const form = document.forms['submit-to-google-sheet'];
-const confirmationMsg = document.getElementById('confirmation-msg');
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    fetch(scriptURL, { 
-        method: 'POST', 
-        body: new FormData(form),
-        mode: 'no-cors' 
-    })
-    .then(response => {
-        if (response.ok) {
-            confirmationMsg.style.display = 'block';
-            form.reset();
-        } else {
-            console.error('Failed to submit form');
-        }
-    })
-    .catch(error => console.error('Error!', error.message));
-});
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwTKm3H6uFpciZimd7EMz1Mh9OF4deZ3qTXm37Ag_yO8wFJtoULKu7SkbkBU9Jlj9wb/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
